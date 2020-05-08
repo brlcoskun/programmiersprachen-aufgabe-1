52f78f441b93bc9530b079849bd09b47324ccf1e
Beril Coskun <berilcoskun@Berils-MBP.fritz.box>
2020-05-08 11:51:37 +0200

    add task 1.3

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+  
+int kleinste_zahl();
 
 int main()
 {
-  std::cout << "Hello, World!\n";
+  std::cout << "Hello, World!\n";   // neue zeile 
+
+  int result = kleinste_zahl();
+  std::cout << "Kleinste Zahl ist: " << result << " \n" ;
   return 0;
+}
+
+int kleinste_zahl()
+{
+  int potential = 20;    // anfangszahl
+  bool found_number = true;  
+  
+  do{
+    found_number = true;
+    for (int i = 2; i < 21; ++i) {
+      if(potential % i != 0){
+        found_number = false;
+        potential+=20;
+      }
+    }
+ 
+    if(found_number == true){
+      return potential;
+
+    }
+
+  }while(!found_number);
+    return -1;
+
 }
